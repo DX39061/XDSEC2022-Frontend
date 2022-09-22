1ea81d01995c5c3faac6aa879329fd695a7504cd
dx3906 <21009201351@stu.xidian.edu.cn>
2022-09-23 01:26:04 +0800

    convert jpg to webp

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,6 @@
   <div class="home">
     <h1>信息安全协会（XDSEC）2022</h1>
     <h1>招新报名系统</h1>
-    <img alt="XDSEC logo" sizes="120" src="../assets/1.jpg">
+    <img alt="XDSEC logo" sizes="120" src="../assets/1.webp">
   </div>
 </template>
